d775d74911e06888d9760ce1f6ba15f82b9dde48
ahmadarifmaulana <arif.maulana@qoala.id>
2026-08-19 16:08:19 +0700

    Adjust exluded user

--- src/constants/leaderboard.ts
+++ src/constants/leaderboard.ts
@@ -1,2 +1,2 @@
 /** Test / internal accounts that must not appear on public leaderboards. */
-export const LEADERBOARD_EXCLUDED_USER_IDS: number[] = [] // [249, 345, 313, 329, 279, 280, 281, 308, 244, 157];
+export const LEADERBOARD_EXCLUDED_USER_IDS: number[] = [249] // [249, 345, 313, 329, 279, 280, 281, 308, 244, 157];
