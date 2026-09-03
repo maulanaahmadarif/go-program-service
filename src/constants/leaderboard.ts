e98fda57518a8ce67cb7bed655c28e2beb746b14
ahmadarifmaulana <arif.maulana@qoala.id>
2026-09-03 09:41:40 +0700

    exclude ids

--- src/constants/leaderboard.ts
+++ src/constants/leaderboard.ts
@@ -1,2 +1,2 @@
 /** Test / internal accounts that must not appear on public leaderboards. */
-export const LEADERBOARD_EXCLUDED_USER_IDS: number[] = [249, 345, 313, 329, 279, 280, 281, 308, 244, 157];
+export const LEADERBOARD_EXCLUDED_USER_IDS: number[] = [346, 249, 313, 347, 345, 329, 279, 280, 281, 348, 308, 244, 157];
